dcd9297076aa3b25b5cc0944aa3a05fda743c8a5
emiliero <emiliero@hiof.no>
2020-03-11 21:38:24 +0100

    Setup login authentication with OAuth2

--- src/main/kotlin/com/github/emiliero/echelon/Main.kt
+++ src/main/kotlin/com/github/emiliero/echelon/Main.kt
@@ -1,20 +1,30 @@
 package com.github.emiliero.echelon
 
+import com.github.emiliero.dicey.BuildConfig
 import discord4j.core.DiscordClient
 import discord4j.core.DiscordClientBuilder
-import discord4j.core.`object`.entity.Message
-import discord4j.core.`object`.entity.MessageChannel
-import discord4j.core.`object`.entity.User
 import discord4j.core.event.domain.lifecycle.ReadyEvent
 import discord4j.core.event.domain.message.MessageCreateEvent
-import java.security.SecureRandom
-
 
 
 class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
+            val client: DiscordClient = DiscordClientBuilder(BuildConfig.TOKEN_KEY).build()
+
+            client.eventDispatcher
+                .on(MessageCreateEvent::class.java)
+                .subscribe { event: MessageCreateEvent ->
+                    event.message.content.ifPresent { c: String? -> println(c) }
+                }
+
+            client.eventDispatcher.on(ReadyEvent::class.java)
+                .subscribe { event: ReadyEvent ->
+                    val self = event.self
+                    println(java.lang.String.format("Logged in as %s#%s", self.username, self.discriminator))
+                }
+
             println("H")
         }
     }
