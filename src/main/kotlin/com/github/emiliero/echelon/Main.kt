7de0ef2a6a9eda240bc4f45d68ea555f8e6663a2
emiliero <emiliero@hiof.no>
2020-03-11 21:31:28 +0100

    Main

---
+++ src/main/kotlin/com/github/emiliero/echelon/Main.kt
@@ -0,0 +1,21 @@
+package com.github.emiliero.echelon
+
+import discord4j.core.DiscordClient
+import discord4j.core.DiscordClientBuilder
+import discord4j.core.`object`.entity.Message
+import discord4j.core.`object`.entity.MessageChannel
+import discord4j.core.`object`.entity.User
+import discord4j.core.event.domain.lifecycle.ReadyEvent
+import discord4j.core.event.domain.message.MessageCreateEvent
+import java.security.SecureRandom
+
+
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("H")
+        }
+    }
+}
