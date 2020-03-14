936945532bfc0e48ec4b1032204361f151605fe9
Anders Saxrud <brannersrud@gmail.com>
2020-03-14 18:19:04 +0100

    added the join/leave and printqueue actions/

---
+++ build/gen/buildconfig/src/main/com/github/emiliero/echelon/BuildConfig.java
@@ -0,0 +1,14 @@
+package com.github.emiliero.echelon;
+
+/** DO NOT EDIT. GENERATED CODE */
+public final class BuildConfig
+{
+   private BuildConfig () { /*. no instance */ }
+
+   public static final String VERSION = "0.1.0";
+
+   public static final String NAME = "Echelon";
+
+   public static final String TOKEN_KEY = "Njg3Mzk4Njg3MzQ1MDgyMzcx.XmlL_A.1XrMUKYpBLnY_l9qs5qZ7Fs5BiM";
+
+}
--- src/main/kotlin/com/github/emiliero/echelon/commands/Join.kt
+++ src/main/kotlin/com/github/emiliero/echelon/commands/Join.kt
@@ -1,5 +1,9 @@
 package com.github.emiliero.echelon.commands
 
+import com.github.emiliero.echelon.commands.list.listActions.addPersonToQueue
+import com.github.emiliero.echelon.commands.list.listActions.printList
+import com.github.emiliero.echelon.commands.list.listActions.removePersonFromQue
+import com.github.emiliero.echelon.model.Person
 import discord4j.core.DiscordClient
 import discord4j.core.`object`.entity.Message
 import discord4j.core.`object`.entity.MessageChannel
@@ -9,9 +13,12 @@ import discord4j.core.event.domain.message.MessageCreateEvent
 fun Join(client: DiscordClient) {
     joinQueue(client)
     leaveQueue(client)
+    printQueue(client)
 }
 
 private fun leaveQueue(client: DiscordClient) {
+    var username : String =""
+    var discriminator : String =""
     client.eventDispatcher.on(MessageCreateEvent::class.java)
         .map { obj: MessageCreateEvent -> obj.message }
         .filter { message: Message ->
@@ -20,12 +27,18 @@ private fun leaveQueue(client: DiscordClient) {
         .filter { message: Message ->
             message.content.orElse("").contains("!leave", ignoreCase = true)
         }
-        .flatMap<MessageChannel> { obj: Message -> obj.channel }
-        .flatMap<Message> { channel: MessageChannel -> channel.createMessage("You've left the queue!") }
+        .flatMap { m : Message ->
+            username = m.author.get().username
+            discriminator = m.author.get().discriminator
+            m.channel
+        }
+        .flatMap<Message> { channel: MessageChannel -> channel.createMessage(removePersonFromQue(username, discriminator))}
         .subscribe()
 }
 
 private fun joinQueue(client: DiscordClient) {
+    var username: String = "";
+    var discriminator: String = "";
     client.eventDispatcher.on(MessageCreateEvent::class.java)
         .map { obj: MessageCreateEvent -> obj.message }
         .filter { message: Message ->
@@ -33,8 +46,35 @@ private fun joinQueue(client: DiscordClient) {
         }
         .filter { message: Message ->
             message.content.orElse("").contains("!join", ignoreCase = true)
+        }.flatMap { m: Message ->
+            username = m.author.get().username
+            discriminator = m.author.get().discriminator
+            m.channel
         }
-        .flatMap<MessageChannel> { obj: Message -> obj.channel }
-        .flatMap<Message> { channel: MessageChannel -> channel.createMessage("You've been added to the queue!") }
+        .flatMap<Message> { channel: MessageChannel ->
+            channel.createMessage(
+                addPersonToQueue(
+                    username,
+                    discriminator
+                )
+            )
+        }
         .subscribe()
 }
+
+    private fun printQueue(client: DiscordClient){
+        client.eventDispatcher.on(MessageCreateEvent::class.java)
+            .map { obj: MessageCreateEvent -> obj.message }
+            .filter { message: Message ->
+                message.author.map { user: User -> !user.isBot}.orElse(false)
+            }
+            .filter { message: Message ->
+                message.content.orElse("").contains("!printQueue", ignoreCase = true)
+            }.flatMap { m : Message -> m.channel }
+            .flatMap<Message> { channel: MessageChannel -> channel.createMessage(printList()) }
+            .subscribe()
+        }
+
+
+
+
