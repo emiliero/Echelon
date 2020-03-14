1e1850b752a7fd9d8a007d47b8f3e2f01cf0b4ee
emiliero <emiliero@hiof.no>
2020-03-14 21:23:27 +0100

    Fixed !leave bug

--- src/main/kotlin/com/github/emiliero/echelon/commands/list/listActions.kt
+++ src/main/kotlin/com/github/emiliero/echelon/commands/list/listActions.kt
@@ -10,36 +10,37 @@ object listActions {
 
 
     fun addPersonToQueue(uName : String, discriminator : String) : String {
-        val studentInQue = checkIfStudentIsInQueue(uName, discriminator);
+        val studentInQue = checkIfStudentIsInQueue(uName, discriminator)
         var message : String = ""
 
         if(!studentInQue){
-            val p : Student = Student(uName, queCount, discriminator);
+            val p : Student = Student(uName, queCount, discriminator)
             if(studentList.any()){
-                studentList.add(studentList.lastIndex, p);
+                studentList.add(studentList.lastIndex, p)
                 message = "${p.name} has been added to the queue you are at spot ${p.placeInQue}"
                 queCount+=1
-            }
-            else{
-                studentList.add(p);
+            } else {
+                studentList.add(p)
                 message ="${p.name} has been added to the queue, you are the first one up at place ${p.placeInQue}"
                 queCount+=1
             }
-        }else{
+        } else {
             message = "${uName} is already in the queue, you can't join twice <:birthdayPepega:688469221499207744>"
         }
-        return message;
+
+        return message
     }
 
     fun removePersonFromQue(uName:String, discriminator: String) : String {
         var student : Student = studentList.get(0)
 
         studentList.removeIf { t: Student ->
-            student=t;
+            student=t
+            queCount-=1
             t.name == uName && t.id == discriminator
         }
 
-        return "${student.name} has been removed from their spot in the queue, spot ${student.placeInQue}";
+        return "${student.name} has been removed from their spot in the queue, spot ${student.placeInQue}"
     }
 
     private fun checkIfStudentIsInQueue(uName: String, discriminator: String) : Boolean{
@@ -48,20 +49,20 @@ object listActions {
 
         iterator.forEach { student ->
            if(student.name == uName && student.id == discriminator){
-               isInQue = true;
+               isInQue = true
            }
         }
-        return isInQue;
+        return isInQue
     }
 
 
     fun printList(): String{
-        var builder :StringBuilder = StringBuilder();
+        var builder : StringBuilder = StringBuilder()
         var iterator = studentList.iterator()
 
         iterator.forEach { student ->
             builder.append(student.placeInQue.toString() + ": " + student.name +"\n")
         }
-        return builder.toString();
+        return builder.toString()
     }
 }
