3bb7c574eb9687fc43fa4a0679e71250924ffc91
emiliero <emiliero@hiof.no>
2020-03-16 19:53:51 +0100

    Added release branch

--- src/main/kotlin/com/github/emiliero/echelon/Main.kt
+++ src/main/kotlin/com/github/emiliero/echelon/Main.kt
@@ -11,7 +11,7 @@ import discord4j.core.DiscordClientBuilder
 fun main(args: Array<String>) {
     val client: DiscordClient = DiscordClientBuilder(BuildConfig.TOKEN_KEY).build()
 
-
+    //Release 1.0
     login(client)
     printHelp(client) //Help command
     queueActions(client) //Queue commands
