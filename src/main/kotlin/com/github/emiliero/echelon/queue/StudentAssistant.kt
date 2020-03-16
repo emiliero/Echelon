00e5a71aff8277e2f02ab32e3da247bf2fa02ae5
emiliero <emiliero@hiof.no>
2020-03-16 22:11:36 +0100

    updated gitignore

---
+++ src/main/kotlin/com/github/emiliero/echelon/queue/StudentAssistant.kt
@@ -0,0 +1,34 @@
+package com.github.emiliero.echelon.queue
+
+enum class StudentAssitant(studentAssistantUsername: String) {
+    LarsEmil("larseknu"){
+        override fun toString(): String {
+            return "8231"
+        }
+    },
+    Anders("Anders"){
+        override fun toString(): String {
+            return "7082"
+        }
+    },
+    Emilie("Zoryi"){
+        override fun toString(): String {
+            return "9995"
+        }
+    },
+    Jo("Joandreas"){
+        override fun toString(): String {
+            return "9781"
+        }
+    },
+    Mette("Mette"){
+        override fun toString(): String {
+            return "7398"
+        }
+    },
+    JornHe("Yearn"){
+        override fun toString(): String {
+            return "3599"
+        }
+    }
+}
