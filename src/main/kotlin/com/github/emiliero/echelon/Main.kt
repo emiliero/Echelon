9418565daf83384ddd15c0318c76131d08d676aa
emiliero <emiliero@hiof.no>
2020-03-25 16:34:53 +0100

    Changed some stuffs for pipeline

--- src/main/kotlin/com/github/emiliero/echelon/Main.kt
+++ src/main/kotlin/com/github/emiliero/echelon/Main.kt
@@ -1,20 +1,13 @@
 package com.github.emiliero.echelon
 
-import com.github.emiliero.echelon.commands.printHelp
-import com.github.emiliero.echelon.login.login
-import com.github.emiliero.echelon.commands.queueActions
-import com.github.emiliero.echelon.queue.ListActionsArray
-import discord4j.core.DiscordClient
-import discord4j.core.DiscordClientBuilder
-
 
 fun main(args: Array<String>) {
-    val client: DiscordClient = DiscordClientBuilder(BuildConfig.TOKEN_KEY).build()
+    /*val client: DiscordClient = DiscordClientBuilder(BuildConfig.TOKEN_KEY).build()
 
     //Release 1.0
     login(client)
     printHelp(client) //Help command
     queueActions(client) //Queue commands
-    client.login().block()
+    client.login().block()*/
 }
 
