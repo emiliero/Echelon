0e694813a68882939155963bac486659dbb98e75
emiliero <emiliero@hiof.no>
2020-08-23 11:08:08 +0200

    Added activity and presence

--- src/main/kotlin/com/github/emiliero/echelon/Main.kt
+++ src/main/kotlin/com/github/emiliero/echelon/Main.kt
@@ -5,9 +5,13 @@ import com.github.emiliero.echelon.commands.printHelp
 import com.github.emiliero.echelon.commands.queueActions
 import discord4j.core.DiscordClient
 import discord4j.core.DiscordClientBuilder
+import discord4j.core.`object`.presence.Activity
+import discord4j.core.`object`.presence.Presence
 
 fun main(args: Array<String>) {
-    val client: DiscordClient = DiscordClientBuilder(BuildConfig.TOKEN_KEY).build()
+    val client: DiscordClient = DiscordClientBuilder(BuildConfig.TOKEN_KEY)
+        .setInitialPresence(Presence.online(Activity.listening("!help")))
+        .build()
 
     login(client)
     printHelp(client) //Help command
